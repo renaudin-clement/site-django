44d7a48bd2ab383fff2dd7cc11185aa17f34b753
renaudin-clement <pro.renaudin.clement@gmail.com>
2025-09-04 13:34:14 +0200

    mise en place de myApp

---
+++ Djangogo/myApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def start(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
